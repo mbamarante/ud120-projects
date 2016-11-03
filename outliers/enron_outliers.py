f85079cd9f5ae2371088679b4746c96f0a1bdf51
Maicon Amarante <maicon.ba@gmail.com>
2016-11-03 19:36:05 -0200

    outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,6 +10,31 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+data_dict.pop("TOTAL", 0)
+
+salary_list = list()
+
+for key, value in data_dict.iteritems():
+    salary_list.append([key, value["salary"]])
+
+import pprint
+pprint.pprint(sorted(salary_list, key=lambda tup: tup[1]))
+#     print("key:", key)
+#     print("value:", value)
+#
+# for person in data_dict:
+#     salary = data_dict[person]["salary"]
+#     bonus = data_dict[person]["bonus"]
+#     #below, we need to ignore NaN values which were removed in the data array but not in
+#     #the dictionary
+#     if salary != 'NaN' and bonus != 'NaN' and salary >= 5000000 and bonus > 1000000:
+#         print(person)
+
+from operator import itemgetter
+#print sorted(data_dict, key=itemgetter('salary'))
+#print sorted(data_dict, key=lambda k: k["salary"])
+
 data = featureFormat(data_dict, features)
 
 
@@ -17,3 +42,15 @@ data = featureFormat(data_dict, features)
 
 
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+
+print data[:,0].max()
+
+26,704,229.0
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,10 +26,11 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
-
-
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+#print reg.coef_
+#print reg.score(ages_test, net_worths_test)
 
 
 
@@ -59,6 +60,7 @@ except NameError:
 
 
 
+
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -78,6 +80,8 @@ if len(cleaned_data) > 0:
     plt.ylabel("net worths")
     plt.show()
 
+    print reg.coef_
+    print reg.score(ages_test, net_worths_test)
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
