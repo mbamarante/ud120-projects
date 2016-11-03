043736298c16bc8f589092190f7643be0465cd7f
Maicon Amarante <maicon.ba@gmail.com>
2016-11-03 18:14:13 -0200

    done explore enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,38 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+import pprint
 
+print "data points:", len(enron_data)
+print "features:", len(enron_data["SKILLING JEFFREY K"])
+
+poi = 0
+for person in enron_data:
+    poi += enron_data[person]["poi"]
+print "poi:", poi
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+pprint.pprint(enron_data["SKILLING JEFFREY K"])
+
+print "Total Payments:"
+print "  SKILLING JEFFREY K", enron_data["SKILLING JEFFREY K"]["total_payments"]
+print "  LAY KENNETH L     ", enron_data["LAY KENNETH L"]["total_payments"]
+print "  FASTOW ANDREW S   ", enron_data["FASTOW ANDREW S"]["total_payments"]
+
+import numpy as np
+
+salary_found = 0
+email_found = 0
+total_payments_not_found = 0
+for person in enron_data:
+    if enron_data[person]["email_address"] != "NaN":
+        email_found += 1
+    if enron_data[person]["salary"] != "NaN":
+        salary_found += 1
+    if enron_data[person]["total_payments"] == "NaN":
+        total_payments_not_found += 1
+
+print "salary found:", salary_found
+print "email found:", email_found
+print "% payments not found:", total_payments_not_found / float(len(enron_data))
