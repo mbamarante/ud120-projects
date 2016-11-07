4da3a5945f22600162243ca6cdcdfb6e279fb655
Maicon Amarante <maicon.ba@gmail.com>
2016-11-07 09:27:09 -0200

    evaluation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -29,3 +29,45 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print "accuracy:", clf.score(features, labels)
+
+
+### cross-validation
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+
+clf = clf.fit(features_train, labels_train)
+
+print "accuracy:", clf.score(features_test, labels_test)
+print "classes (0 = not poi / 1 = poi):", clf.classes_
+
+### evaluation
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+labels_pred = clf.predict(features_test)
+
+print classification_report(labels_test, labels_pred)
+
+print "confusion matrix:"
+print confusion_matrix(labels_test, labels_pred)
+
+### precision and recall
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+print "precision score:", precision_score(labels_test, labels_pred)
+print "recall score:", recall_score(labels_test, labels_pred)
+
+
+### teste ###
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+print confusion_matrix(true_labels, predictions)
+print "precision score:", precision_score(true_labels, predictions)
+print "recall score:", recall_score(true_labels , predictions)
